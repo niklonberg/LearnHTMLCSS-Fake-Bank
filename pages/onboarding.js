fe6fcdc6b6addb53059b574294be03a496d1cc63
Nikolai Lønberg <niklonberg@gmail.com>
2023-09-22 11:59:58 +0200

    add slider scroll event, to update active dot if user uses scrollbar instead of dots

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -1,16 +1,18 @@
 const slider = document.querySelector("#slider");
 const dotsContainer = document.querySelector("#dots");
-const dots = document.querySelectorAll(".dot");
+const dots = document.querySelectorAll("#dots .dot");
 
 dotsContainer.addEventListener("click", (event) => {
   if (event.target.classList.contains("dot")) {
     dots.forEach((dot) => {
       dot.classList.remove("active");
     });
-    event.target.classList.toggle("active");
+    event.target.classList.add("active");
   }
 });
 
-slider.addEventListener("scroll", (event) => {
-  console.log(event);
+slider.addEventListener("scroll", () => {
+  const visibleSlideIndex = Math.round(slider.scrollLeft / slider.clientWidth);
+  dots.forEach((dot) => dot.classList.remove("active"));
+  dots[visibleSlideIndex].classList.add("active");
 });
