6400203c9f23402d1670a6b9b7ac8a834986fcd9
Nikolai Lønberg <niklonberg@gmail.com>
2023-09-26 09:58:26 +0200

    adding build again?

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/LearnHTMLCSS-Fake-Bank",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        onboarding: resolve(__dirname, "onboarding.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        profile: resolve(__dirname, "profile.html"),
+        services: resolve(__dirname, "services.html"),
+        signin: resolve(__dirname, "signin.html"),
+        transactions: resolve(__dirname, "transactions.html"),
+      },
+    },
+  },
 });
