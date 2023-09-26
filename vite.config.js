abf318bd1dbacc4d5138620b921db0ce26987422
Nikolai Lønberg <niklonberg@gmail.com>
2023-09-26 10:21:45 +0200

    i just love file paths so much

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        onboarding: resolve(__dirname, "/pages/onboarding.html"),
-        dashboard: resolve(__dirname, "/pages/dashboard.html"),
-        profile: resolve(__dirname, "/pages/profile.html"),
-        services: resolve(__dirname, "/pages/services.html"),
-        signin: resolve(__dirname, "/pages/signin.html"),
-        transactions: resolve(__dirname, "/pages/transactions.html"),
+        onboarding: resolve(__dirname, "pages/onboarding.html"),
+        dashboard: resolve(__dirname, "pages/dashboard.html"),
+        profile: resolve(__dirname, "pages/profile.html"),
+        services: resolve(__dirname, "pages/services.html"),
+        signin: resolve(__dirname, "pages/signin.html"),
+        transactions: resolve(__dirname, "pages/transactions.html"),
       },
     },
   },
