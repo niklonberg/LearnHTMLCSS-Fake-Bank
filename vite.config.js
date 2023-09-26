a0223af054a7f420f0a2e10c39cc35192c9ad7a2
Nikolai Lønberg <niklonberg@gmail.com>
2023-09-26 10:10:46 +0200

    try with base inside build

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,18 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/LearnHTMLCSS-Fake-Bank",
+  build: {
+    base: "/LearnHTMLCSS-Fake-Bank",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        onboarding: resolve(__dirname, "onboarding.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        profile: resolve(__dirname, "profile.html"),
+        services: resolve(__dirname, "services.html"),
+        signin: resolve(__dirname, "signin.html"),
+        transactions: resolve(__dirname, "transactions.html"),
+      },
+    },
+  },
 });
